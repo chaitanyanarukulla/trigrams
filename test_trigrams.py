28296cffc464db743f070917e28eb63e1c0c28e6
Marco Zangari <mjzangari@gmail.com>
2017-10-20 15:16:03 -0700

    test_read_file2 passes a length that is longer than 100

--- test_trigrams.py
+++ test_trigrams.py
@@ -2,11 +2,11 @@
 import pytest
 
 def test_read_file():
-	"""Tests the read_file function in trigrams.py."""
+	"""Tests the read_file function in trigrams.py for whether a string."""
 	from trigrams import read_file
 	assert type(read_file("text.txt")) == str
 
-# def test_read_file2():
-# """."""
-
-
+def test_read_file2():
+	"""Tests the read_file function in trigrams.py for length of text."""
+	from trigrams import read_file
+	assert len(read_file("text.txt")) > 100
