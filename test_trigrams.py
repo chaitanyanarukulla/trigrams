b4f48217d354d8480a02494acb671b2a24d99cd7
Chaitanya.Narukulla <Chai@Ashleys-MacBook-Air.local>
2017-10-21 13:26:06 -0700

    Re-factored and fixed bugs to meet user stories

--- test_trigrams.py
+++ test_trigrams.py
@@ -5,13 +5,3 @@ def test_read_file():
 	"""Tests the read_file function in trigrams.py for whether a string."""
 	from trigrams import read_file
 	assert type(read_file("text.txt")) == str
-
-def test_read_file2():
-	"""Tests the read_file function in trigrams.py for length of text."""
-	from trigrams import read_file
-	assert len(read_file("text.txt")) > 100
-
-def test_change_string_text_to_list():
-	from trigrams import change_string_text_to_list
-	assert len(change_string_text_to_list("string of words")) < 100
-
